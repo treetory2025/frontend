7a05ebd0e2b8b3677d92813660565712a526922f
jinyoung jung <ya_1001@naver.com>
2025-12-03 14:44:28 +0900

    feat:prettier 설정

--- treetory/eslint.config.mjs
+++ treetory/eslint.config.mjs
@@ -13,6 +13,24 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  //  Prettier 통합 구간
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    extends: [
+      prettierConfig, // ESLint와 Prettier 충돌 규칙 제거
+    ],
+    rules: {
+      "prettier/prettier": "error", // Prettier 포맷팅 오류를 ESLint 오류로 처리
+      "react/self-closing-comp": "error",
+
+      // Next 기본 no-unused-vars 꺼주고 TS 버전 사용
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
 ]);
 
 export default eslintConfig;
