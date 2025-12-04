caeb6e002262285cd0fe082b301c5ae3e8159e69
jinyoung jung <ya_1001@naver.com>
2025-12-04 12:01:14 +0900

    feat:main-content layout

--- treetory/app/layout.tsx
+++ treetory/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import Header from "@/components/commons/Header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Treetory 2025",
@@ -30,7 +19,7 @@ export default function RootLayout({
           <header>
             <Header />
           </header>
-          {children}
+          <main className="h-full">{children}</main>
         </div>
       </body>
     </html>
