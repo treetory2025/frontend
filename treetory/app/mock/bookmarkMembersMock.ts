bca487bf8c36a6e94050ec2b5d2ecb45889b8e95
Jung Jinyoung <ya_1001@naver.com>
2025-12-06 10:11:56 +0900

    Feat/bookmarks UI (#5)
    * refactor:menu-folder, routing & feat:connect to bookmarks-page
    
    * feat:Next 16.0.7 version install (보안 이슈)
    
    * refactor: height delete
    
    * feat: common SearchInput UI
    
    * feat : 임시 목업 데이터로 즐겨찾기 UI 구현 (페이지네이션 완성)

---
+++ treetory/app/mock/bookmarkMembersMock.ts
@@ -0,0 +1,20 @@
+export const bookmarksMembersMock = {
+  members: {
+    content: [
+      { memberId: "1", nickname: "테스트유저1", email: "test1@test.com" },
+      { memberId: "2", nickname: "감자123", email: "potato@test.com" },
+      { memberId: "3", nickname: "쫑이", email: "dog@test.com" },
+      { memberId: "4", nickname: "헤이즐넛", email: "hazel@test.com" },
+      { memberId: "5", nickname: "라떼소년", email: "latte@test.com" },
+      { memberId: "6", nickname: "포메공주", email: "pome@test.com" },
+      { memberId: "7", nickname: "햄찌러버", email: "ham@test.com" },
+      { memberId: "8", nickname: "치즈볼초이", email: "cheese@test.com" },
+      { memberId: "9", nickname: "문어🐙", email: "octo@test.com" },
+      { memberId: "10", nickname: "야옹선생", email: "cat@test.com" },
+    ],
+    pageNum: 0,
+    pageSize: 10,
+    totalPage: 1,
+    totalElements: 10,
+  },
+};
