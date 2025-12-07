8d679b911c36966575bcddb23faf8f1f366cb0f7
jinyoung jung <ya_1001@naver.com>
2025-12-08 03:02:15 +0900

    refactor: 반응형 높이 수정

--- treetory/app/(header)/(menu)/settings/theme/page.tsx
+++ treetory/app/(header)/(menu)/settings/theme/page.tsx
@@ -7,25 +7,31 @@ import ContentSection from "@/components/commons/ContentSection";
 import PageHeading from "@/components/commons/PageHeading";
 import Tab from "@/components/commons/Tab";
 import BackgroundContainer from "@/components/ui/settings/Theme/BackgroundContainer";
+import TreeContainer from "@/components/ui/settings/Theme/TreeContaier";
 
 export default function Page() {
   const THEME_TABS = [
     { label: "배경", value: "background" },
-    { label: "트리", value: "theme" },
+    { label: "트리", value: "tree" },
   ];
   const [theme, setTheme] = useState(THEME_TABS[0].value);
   const user = useUserStore((s) => s.user);
 
   const [background, setBackground] = useState(user?.background || "고요한 밤");
+  const [tree, setTree] = useState(user?.theme || "눈 덮인 트리");
+
   //  api 연결 전 임시 로직
   const handleBackGroundUpdate = (newTheme: string) => {
     setBackground(newTheme);
+  };
+  const handleTreeUpdate = (newTheme: string) => {
+    setTree(newTheme);
   };
 
   return (
     <>
       <PageHeading title="테마 설정" />
-      <ContentSection className="flex h-full flex-col gap-3">
+      <ContentSection className="flex h-full flex-col gap-3 md:h-dvh">
         <Tab options={THEME_TABS} value={theme} onChange={setTheme} />
 
         {theme === "background" && (
@@ -37,6 +43,18 @@ export default function Page() {
               <p className="text-navy text-subtitle">{background}</p>
             </div>
             <BackgroundContainer onSubmit={handleBackGroundUpdate} />
+          </>
+        )}
+
+        {theme === "tree" && (
+          <>
+            <div className="flex flex-col rounded-xl bg-white p-4">
+              <p className="text-caption md:text-body text-muted-navy">
+                현재 적용된 테마
+              </p>
+              <p className="text-navy text-subtitle">{tree}</p>
+            </div>
+            <TreeContainer onSubmit={handleTreeUpdate} />
           </>
         )}
       </ContentSection>
