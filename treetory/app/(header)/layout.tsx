d83071f5eba5db295e7bac5fa1b3fbc49492c103
jinyoung jung <ya_1001@naver.com>
2025-12-10 19:15:23 +0900

    refactor: 테마 반영을 위한 글로벌 레이아웃 변경 & feat : header padding 반응형 추가

--- treetory/app/(header)/layout.tsx
+++ treetory/app/(header)/layout.tsx
@@ -1,15 +1,37 @@
+"use client";
+
 import Header from "@/components/commons/Header";
+import { usePathname } from "next/navigation";
+import { useOwnerStore } from "@/store/userStore";
 
 export default function HeaderLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isTreePage = pathname.startsWith("/tree");
+
+  // Zustand store
+  const { owner, _hasHydrated } = useOwnerStore();
+
+  // Hydration 전에 렌더하면 깜빡임 발생 → 방지
+  if (!_hasHydrated) return null;
+
+  // tree 페이지이고 owner가 있을 때만 배경 적용
+  const bgClass =
+    isTreePage && owner
+      ? owner.treeBackground === "SILENT_NIGHT"
+        ? "bg-navy"
+        : "bg-skyblue"
+      : "";
+
   return (
-    <div className="flex h-full flex-col justify-start gap-8">
+    <div className={`flex h-full flex-col justify-start gap-8 ${bgClass}`}>
       <header>
         <Header />
       </header>
+
       {children}
     </div>
   );
