178c358f5df3fa92c831e9229e6901a80c8756bd
jinyoung jung <ya_1001@naver.com>
2025-12-10 18:52:37 +0900

    refactor: 폴더 이름 변경

---
+++ treetory/store/userStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import type { User } from "@/types/user";
+import { persist } from "zustand/middleware";
+
+interface UserState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  clearUser: () => void;
+  _hasHydrated: boolean;
+  setHasHydrated: (state: boolean) => void;
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+      _hasHydrated: false,
+      setHasHydrated: (state) => {
+        set({
+          _hasHydrated: state,
+        });
+      },
+    }),
+    {
+      name: "user-storage", // localStorage key
+      onRehydrateStorage: () => (state) => {
+        state?.setHasHydrated(true);
+      },
+    },
+  ),
+);
