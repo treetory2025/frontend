7f40ca28ecf92f7af6faea5eac3773736d67f1a4
jinyoung jung <ya_1001@naver.com>
2025-12-12 01:43:36 +0900

    feat: 트리 추가 버튼 UI , 사용자 확인 후 트리 추가 기능 구현

--- treetory/components/ui/tree/Header.tsx
+++ treetory/components/ui/tree/Header.tsx
@@ -1,23 +1,32 @@
 "use client";
 import { useOwner } from "@/app/(header)/tree/[uuid]/tree-context";
+import { useUserStore } from "@/store/userStore";
 import { List, RotateCw } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import AddTreeButton from "./Button";
 
 export default function TreeHeader() {
   const { owner, refreshOwner } = useOwner();
+  const user = useUserStore().user;
+
   const router = useRouter();
   const params = useParams();
   const uuid = params.uuid as string;
 
+  const isOwner = user?.uuid === uuid;
+
   return (
     <header className="flex flex-col gap-2 select-none">
-      <div>
-        <p className="font-memoment text-beige text-body md:text-xl">
-          새로운 장식을 확인해보세요!
-        </p>
-        <h1 className="text-title font-memoment text-beige">
-          {owner.nickname}님의 <span className="text-green">트리토리</span>
-        </h1>
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="font-memoment text-beige text-body md:text-xl">
+            새로운 장식을 확인해보세요!
+          </p>
+          <h1 className="text-title font-memoment text-beige">
+            {owner.nickname}님의 <span className="text-green">트리토리</span>
+          </h1>
+        </div>
+        {!isOwner && <AddTreeButton uuid={uuid} />}
       </div>
 
       <div className="bg-skyblue/20 text-beige text-caption flex w-full items-center justify-between rounded-md px-6 py-1.5">
