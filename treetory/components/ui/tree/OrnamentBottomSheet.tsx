eb40fd2dc9b6e54b972c31785bdfc92c8fea1e20
jinyoung jung <ya_1001@naver.com>
2025-12-12 18:35:06 +0900

    refactor: isOwner 주석 제거

--- treetory/components/ui/tree/OrnamentBottomSheet.tsx
+++ treetory/components/ui/tree/OrnamentBottomSheet.tsx
@@ -30,8 +30,7 @@ export default function OrnamentBottomSheet({
   ornament,
 }: OrnamentBottomSheetProps) {
   const { owner, uuid } = useOwner();
-  // const isOwner = owner ? isUser(uuid) : false;
-  const isOwner = true;
+  const isOwner = owner ? isUser(uuid) : false;
   const isChristmas = isChristmas2025InKorea();
 
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
