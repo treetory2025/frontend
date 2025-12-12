8652aa84f9e0389604ea57c31f53bde548d11c69
jinyoung jung <ya_1001@naver.com>
2025-12-12 16:37:50 +0900

    refactor: 장식 날짜 포맷팅

--- treetory/components/ui/tree/OrnamentBottomSheet.tsx
+++ treetory/components/ui/tree/OrnamentBottomSheet.tsx
@@ -14,12 +14,13 @@ export default function OrnamentBottomSheet({
   ornament,
 }: OrnamentBottomSheetProps) {
   if (!ornament) return null;
+  const [year, month, day] = ornament.createdDate.split(".");
 
   return (
     <BottomSheet isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col items-center gap-6">
-        <div className="relative flex items-center justify-center rounded-full bg-muted-bg size-30">
-          <div className="w-20 h-20 overflow-hidden rounded-full">
+        <div className="bg-muted-bg relative flex size-30 items-center justify-center rounded-full">
+          <div className="h-20 w-20 overflow-hidden rounded-full">
             <Image
               alt="선택된 장식 이미지"
               src={ornament.imgUrl}
@@ -30,17 +31,17 @@ export default function OrnamentBottomSheet({
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <p className="font-bold text-body text-green">방문자</p>
+          <p className="text-body text-green font-bold">방문자</p>
           <h3 className="text-heading text-fg-primary">
             {ornament.writerNickname}님
           </h3>
-          <p className="pt-1 text-caption text-muted-navy">
-            {ornament.createdDate}에 장식을 등록하였습니다.
+          <p className="text-caption text-muted-navy pt-1">
+            {year}년 {month}월 {day}일에 장식을 등록하였습니다.
           </p>
         </div>
       </div>
 
-      <div className="flex flex-col w-full gap-2">
+      <div className="flex w-full flex-col gap-2">
         <ActionButton onClick={onClose}>확인</ActionButton>
       </div>
     </BottomSheet>
