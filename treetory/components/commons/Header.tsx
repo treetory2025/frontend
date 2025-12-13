4a8b8f0b698b85098d4dd49b39c81119c7c5de50
jinyoung jung <ya_1001@naver.com>
2025-12-14 05:04:06 +0900

    feat: 크리스마스 확인에 따른 배너 변경

--- treetory/components/commons/Header.tsx
+++ treetory/components/commons/Header.tsx
@@ -3,15 +3,30 @@
 import menuIcon from "@/public/icons/menu(default).svg";
 import cameraIcon from "@/public/icons/camera.svg";
 import rudolphIcon from "@/public/icons/rudolph.png";
+import santaIcon from "@/public/icons/santa.png";
+
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 import HeaderMenu from "@/components/ui/menu/HeaderMenu";
 import useCloseOnOutsideOrEsc from "@/hooks/useCloseOnOutsideOrEsc";
+import {
+  getDaysUntilChristmas2025InKorea,
+  isChristmas2025InKorea,
+} from "@/lib/date";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [diffdays, setDiffdays] = useState(0);
   const menuRef = useRef<HTMLDivElement | null>(null);
+  // const isChristmas = isChristmas2025InKorea();
+  const isChristmas = true;
+  useEffect(() => {
+    if (isChristmas) return;
+    const diff = getDaysUntilChristmas2025InKorea();
+    setDiffdays(diff);
+  }, [isChristmas]);
 
   useCloseOnOutsideOrEsc(menuRef, {
     isOpen: isMenuOpen,
@@ -42,12 +57,64 @@ export default function Header() {
         </button>
       </div>
       {/* 크리스마스 디데이 안내 */}
-      <div className="bg-beige flex h-10 items-center justify-center gap-2 rounded-full px-4">
-        <Image src={rudolphIcon} alt="rudolph" />
-        <p className="text-caption text-fg-primary">
-          크리스마스까지? <span className="font-bold">D-22</span>
-        </p>
-      </div>
+      {isChristmas ? (
+        <ChristmasBanner isChristmas={isChristmas} />
+      ) : (
+        <div className="bg-beige flex h-10 items-center justify-center gap-2 rounded-full px-4">
+          <Image src={rudolphIcon} alt="rudolph" />
+          <p className="text-caption text-fg-primary">
+            크리스마스까지? <span className="font-bold"> D-{diffdays}</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
+
+type Props = {
+  isChristmas: boolean;
+};
+
+export const ChristmasBanner = ({ isChristmas }: Props) => {
+  return (
+    <motion.div
+      animate={
+        isChristmas
+          ? {
+              boxShadow: [
+                "0px 0px 20px 7px rgba(32,159,135,0.25)",
+                "0px 0px 30px 12px rgba(32,159,135,0.45)",
+                "0px 0px 20px 7px rgba(32,159,135,0.25)",
+              ],
+            }
+          : {}
+      }
+      transition={
+        isChristmas
+          ? {
+              duration: 1.8,
+              repeat: Infinity,
+              ease: "easeInOut",
+            }
+          : undefined
+      }
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 6,
+        width: 195,
+        height: 40,
+        padding: "0 16px",
+        background: "#209F87",
+        borderRadius: 100,
+      }}
+    >
+      {/* 아이콘 */}
+      <Image src={santaIcon} alt="santa" />
+
+      {/* 텍스트 */}
+      <span className="text-body text-beige">메리 크리스마스!</span>
+    </motion.div>
+  );
+};
