5e16e9a028515d1f8b119cf765c6e29b25f1139f
jinyoung jung <ya_1001@naver.com>
2025-12-13 21:22:14 +0900

    feat:사용자 로그인 여부에 따른 메뉴 UI 구현

--- treetory/components/ui/menu/HeaderMenu.tsx
+++ treetory/components/ui/menu/HeaderMenu.tsx
@@ -1,20 +1,38 @@
 import { MenuItem } from "@/components/ui/menu/MunuItem";
 
-import { MoveRight } from "lucide-react";
-import { Copy } from "lucide-react";
-import { LogIn } from "lucide-react";
-import { Search } from "lucide-react";
+import { MoveRight, Copy, LogIn, LogOut, Search } from "lucide-react";
 
 import { useRouter } from "next/navigation";
+import { isLoggedIn } from "@/lib/auth";
+import { useUserStore } from "@/store/userStore";
+
+type Menu =
+  | {
+      label: string;
+      icon: any;
+      onClick?: () => void;
+      disabled?: boolean;
+    }
+  | {
+      divider: true;
+    };
 
 export default function HeaderMenu({ onClose }: { onClose: () => void }) {
   const router = useRouter();
+  const loggedIn = isLoggedIn();
+  const user = useUserStore((s) => s.user);
+  const clearUser = useUserStore((s) => s.clearUser);
 
-  const menus = [
+  const menus: Menu[] = [
     {
       label: "설정",
       icon: MoveRight,
+      disabled: !loggedIn,
       onClick: () => {
+        if (!loggedIn) {
+          alert("로그인이 필요합니다.");
+          return;
+        }
         router.push("/settings");
         onClose();
       },
@@ -22,35 +40,88 @@ export default function HeaderMenu({ onClose }: { onClose: () => void }) {
     {
       label: "초대링크 복사하기",
       icon: Copy,
+      disabled: !loggedIn,
       onClick: () => {
+        if (!loggedIn) {
+          alert("로그인이 필요합니다.");
+          return;
+        }
         navigator.clipboard.writeText(window.location.href);
-        console.log("초대링크 복사");
         onClose();
       },
     },
-    {
-      label: "로그인",
-      icon: LogIn,
-      onClick: () => {
-        console.log("로그인 페이지 이동");
-        onClose();
-      },
-    },
-    { divider: true, label: "", icon: () => null },
+
+    // 로그인 / 로그아웃 스위칭
+    loggedIn
+      ? {
+          label: "로그아웃",
+          icon: LogOut,
+          onClick: async () => {
+            console.log("로그아웃 처리");
+            try {
+              const res = await fetch(`/api/auth/logout`, {
+                credentials: "include",
+              });
+
+              if (!res.ok) {
+                console.log("로그아웃 요청 실패", res);
+                onClose();
+                return;
+              }
+
+              clearUser();
+              // 해당 페이지 새로고침
+              router.refresh();
+              onClose();
+            } catch (error) {
+              console.error("api 오류", error);
+              onClose();
+            }
+          },
+        }
+      : {
+          label: "로그인",
+          icon: LogIn,
+          onClick: () => {
+            // 로그인 페이지 이동
+            router.push("/login");
+            onClose();
+          },
+        },
+
+    { divider: true },
+
     {
       label: "나의 트리토리 이동",
       icon: MoveRight,
+      disabled: !loggedIn,
       onClick: () => {
-        console.log("나의 트리토리 이동");
+        if (!loggedIn) {
+          alert("로그인이 필요합니다.");
+          return;
+        }
+
+        if (!user) {
+          console.log("유저 인식 실패");
+          return;
+        }
+        router.push(`/tree/${user.uuid}`);
+        onClose();
       },
     },
-    { divider: true, label: "", icon: () => null },
+
+    { divider: true },
+
     {
       label: "즐겨찾기",
       icon: MoveRight,
+      disabled: !loggedIn,
       onClick: () => {
+        if (!loggedIn) {
+          alert("로그인이 필요합니다.");
+          return;
+        }
         router.push("/bookmarks");
-
         onClose();
       },
     },
@@ -58,19 +129,28 @@ export default function HeaderMenu({ onClose }: { onClose: () => void }) {
       label: "사용자 검색",
       icon: Search,
       onClick: () => {
-        console.log("사용자검색 페이지 이동");
+        console.log("사용자 검색 클릭");
+        // router.push("/search");
         onClose();
       },
     },
   ];
 
+  console.log(isLoggedIn());
+
   return (
     <div className="text-fg-primary flex w-full flex-col gap-1 px-2">
       {menus.map((item, idx) =>
-        item.divider ? (
+        "divider" in item ? (
           <div key={idx} className="border-green w-full border-t" />
         ) : (
-          <MenuItem key={item.label} {...item} />
+          <MenuItem
+            key={item.label}
+            {...item}
+            className={
+              item.disabled ? "cursor-not-allowed opacity-30" : "cursor-pointer"
+            }
+          />
         ),
       )}
     </div>
