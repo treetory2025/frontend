a5c4a97f8bbb3d13f8c84bbbf6f31e0e13b2902c
jinyoung jung <ya_1001@naver.com>
2025-12-18 16:36:59 +0900

    refactor:문구 변경

--- treetory/components/ui/tree/Header.tsx
+++ treetory/components/ui/tree/Header.tsx
@@ -43,7 +43,7 @@ export default function TreeHeader() {
         {isPlacement ? (
           <>
             <p>
-              장식이 완료되면 우측 하단
+              우측 하단
               <span className="font-bold"> 장식 완료</span> 버튼을 눌러주세요!
             </p>
             <div className="flex items-center gap-3 md:gap-4">
