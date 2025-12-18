86cb4b138bb739aa145db26a5c80b1b551ead3c8
jinyoung jung <ya_1001@naver.com>
2025-12-19 04:23:28 +0900

    refactor:bgm 버튼 위치 변경

--- treetory/components/ui/menu/BGMButton.tsx
+++ treetory/components/ui/menu/BGMButton.tsx
@@ -8,12 +8,12 @@ export default function BGMButton() {
   return (
     <button
       onClick={toggle}
-      className="text-caption fixed right-4 bottom-4 z-50 rounded-full bg-black/60 px-4 py-2 text-white"
+      className="text-caption fixed bottom-4 left-4 z-100 rounded-full bg-black/60 px-4 py-2 text-white"
     >
       {enabled ? (
         <div className="flex items-center gap-2">
           <Volume2 size={16} />
-          <p>"BGM ON"</p>
+          <p>BGM ON</p>
         </div>
       ) : (
         <div className="flex items-center gap-2">
