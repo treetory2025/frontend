ad5c9ccf4983f3b87196a3e0f83f9d86039fd012
Jung Jinyoung <ya_1001@naver.com>
2025-12-20 03:52:18 +0900

    refactor:og 메타 데이터 수정 & 트리 페이지 메타 데이터 추가 (#67)
    * fix:meta og url 수정
    
    * feat:트리 페이지 og 메타 데이터 추가
    
    * refactor:site_url 개발 사이트 주소로 고정

--- treetory/app/(header)/tree/[uuid]/layout.tsx
+++ treetory/app/(header)/tree/[uuid]/layout.tsx
@@ -2,6 +2,7 @@ import { getTreeOwner } from "@/lib/api";
 import { Owner } from "@/types/user";
 import { OwnerProvider } from "@/app/(header)/tree/[uuid]/tree-context";
 import TreeHeader from "@/components/ui/tree/Header";
+import { Metadata } from "next";
 
 export default async function Layout({
   children,
@@ -22,4 +23,46 @@ export default async function Layout({
       </div>
     </OwnerProvider>
   );
+}
+
+type MetadataProps = {
+  params: { uuid: string };
+};
+
+export async function generateMetadata({
+  params,
+}: MetadataProps): Promise<Metadata> {
+  const { uuid } = params;
+  const owner = (await getTreeOwner(uuid)) as Owner;
+
+  const SITE_URL = "https://develop.bacinf.com";
+
+  const title = `${owner.nickname}의 트리 | 트리토리`;
+  const description = `${owner.nickname}와 함께 만드는 크리스마스 이야기`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `${SITE_URL}/tree/${uuid}`,
+      siteName: "트리토리",
+      locale: "ko_KR",
+      images: [
+        {
+          url: `${SITE_URL}/og-image.png`, // 트리별 이미지 없을 경우 fallback
+          width: 1200,
+          height: 630,
+          alt: `${owner.nickname}의 트리`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [`${SITE_URL}/og-image.png`],
+    },
+  };
 }
