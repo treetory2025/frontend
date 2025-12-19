e12928e59f1daef1c9d89f1cb8023ffdfbfca4c8
jinyoung jung <ya_1001@naver.com>
2025-12-20 03:29:18 +0900

    feat:meta og 값 설정

--- treetory/app/layout.tsx
+++ treetory/app/layout.tsx
@@ -14,9 +14,31 @@ const treetoryFont = localFont({
   variable: "--font-memoment",
 });
 
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 export const metadata: Metadata = {
   title: "Treetory 2025",
   description: "함께 만드는 우리만의 크리스마스 이야기, 트리토리",
+  openGraph: {
+    title: "Treetory 2025",
+    description: "함께 만드는 우리만의 크리스마스 이야기, 트리토리",
+    url: SITE_URL, // 배포 사이트 연결 전
+    siteName: "트리토리",
+    locale: "ko_KR",
+    images: [
+      {
+        url: `${SITE_URL}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "Treetory 2025",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Treetory 2025",
+    description: "함께 만드는 우리만의 크리스마스 이야기, 트리토리",
+    images: [`${SITE_URL}/og-image.png`],
+  },
 };
 
 export const viewport: Viewport = {
