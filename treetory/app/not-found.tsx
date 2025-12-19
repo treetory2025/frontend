1a8821dbad4378392ffa0b43d9870cd9dbde1b28
jinyoung jung <ya_1001@naver.com>
2025-12-19 21:57:23 +0900

    refactor:404 페이지 ui 수정

--- treetory/app/not-found.tsx
+++ treetory/app/not-found.tsx
@@ -1,22 +1,29 @@
-import Link from "next/link";
+"use client";
+
+import { Frown } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function NotFoundPage() {
+  const router = useRouter();
   return (
     <div className="my-auto flex h-full w-full flex-col items-center justify-center gap-4">
-      <div className="bg-beige px-4 text-center">
+      <div className="px-4 text-center">
+        <div className="text-fg-primary flex w-full justify-center">
+          <Frown size={40} />
+        </div>
         <h1 className="text-title text-fg-primary">404</h1>
-        <p className="text-subtitle text-fg-primary">
+        <p className="text-subtitle text-fg-primary font-medium">
           요청하신 페이지를 찾을 수 없습니다.
         </p>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <Link
-          href={"/"}
-          className="text-green text-subtitle w-full border-b-2 text-center font-bold"
+        <button
+          onClick={() => router.push("/")}
+          className="text-beige text-subtitle bg-green w-full cursor-pointer rounded-xl p-2 text-center font-bold"
         >
           홈으로 이동하기
-        </Link>
-        <p className="text-green/50 text-body">
+        </button>
+        <p className="text-green/50 text-body mt-2">
           로그인이 필요한 경우 로그인 페이지로 이동합니다.
         </p>
       </div>
