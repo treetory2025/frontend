2a407424bd00c66a2eec561fe1e415e11fdedb49
Jung Jinyoung <ya_1001@naver.com>
2025-12-20 13:40:48 +0900

    refactor: 인덱스 페이지 메타 데이터 수정, 초대 링크 메타 데이터 추가 #68 (#69)
    * feat: 초대링크 og 메타 데이터 설정
    
    * reacto:사용하지 않는 import 제거

--- treetory/app/(header)/tree/[uuid]/page.tsx
+++ treetory/app/(header)/tree/[uuid]/page.tsx
@@ -1,4 +1,35 @@
 import TreePage from "@/app/(header)/tree/[uuid]/TreePage";
+import { getTreeOwner } from "@/lib/api";
+import { Metadata } from "next";
+
+type Props = {
+  params: { uuid: string };
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `트리토리 초대 링크 도착!`,
+    description: `함께 만드는 우리의 크리스마스 이야기`,
+    openGraph: {
+      title: `트리토리 초대 링크`,
+      description: `함께 만드는 우리의 크리스마스 이야기`,
+      images: [
+        {
+          url: "https://develop.bacinf.com/og-invite-image.png",
+          width: 1200,
+          height: 630,
+          alt: "트리토리 크리스마스 초대",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `트리토리 초대 링크`,
+      description: `함께 만드는 우리의 크리스마스 이야기`,
+      images: ["https://develop.bacinf.com/og-invite-image.png"],
+    },
+  };
+}
 
 export default function Page() {
   return <TreePage />;
