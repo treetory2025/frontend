2b4c011d87ccc4589460d68ceb55bdd0fdc46f08
jinyoung jung <ya_1001@naver.com>
2025-12-21 14:00:56 +0900

    refactor:메뉴 레이아웃 최소 높이 수정

--- treetory/app/(header)/(menu)/layout.tsx
+++ treetory/app/(header)/(menu)/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="no-scrollbar relative flex h-full w-full flex-col overflow-y-auto">
+    <div className="no-scrollbar relative flex min-h-dvh w-full flex-col overflow-y-auto">
       {children}
     </div>
   );
--- treetory/app/(header)/(menu)/tree/[uuid]/decorate/nickname/page.tsx
+++ treetory/app/(header)/(menu)/tree/[uuid]/decorate/nickname/page.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import Header from '@/components/ui/decorate/nickname/Header';
-import NicknameRegisterPage from './NicknameRegisterPage';
+import Header from "@/components/ui/decorate/nickname/Header";
+import NicknameRegisterPage from "./NicknameRegisterPage";
 
 export default function Page() {
   return (
-    <div className="flex flex-col h-screen bg-light-blue">
+    <div className="bg-light-blue flex flex-col">
       <Header title="장식하기" />
       <NicknameRegisterPage />
     </div>
