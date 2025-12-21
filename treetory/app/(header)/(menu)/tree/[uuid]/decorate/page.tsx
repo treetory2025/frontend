75123d057c38b8ceb798ac23b4c73bed03d30981
jinyoung jung <ya_1001@naver.com>
2025-12-21 14:01:35 +0900

    fix:position absolute 제거 후 화면 반응형 로직 추가

--- treetory/app/(header)/(menu)/tree/[uuid]/decorate/page.tsx
+++ treetory/app/(header)/(menu)/tree/[uuid]/decorate/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import PageHeading from '@/components/commons/PageHeading';
-import DecoratePage from './DecoratePage';
+import { useState } from "react";
+import PageHeading from "@/components/commons/PageHeading";
+import DecoratePage from "./DecoratePage";
 
 export default function Page() {
   return (
-    <div className="flex flex-col h-screen bg-light-blue">
-      <PageHeading title="장식하기"/>
+    <div className="bg-light-blue flex h-full flex-col overflow-y-auto">
+      <PageHeading title="장식하기" />
       <DecoratePage />
     </div>
   );
